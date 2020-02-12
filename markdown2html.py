623e0209b22a0fe1e8edcf5fdf6c537a81917c00
scurry222 <scurry222@gmail.com>
2020-02-12 13:52:16 -0800

    fix exit status

--- markdown2html.py
+++ markdown2html.py
@@ -15,12 +15,13 @@ def main(argv):
     """
     if len(argv) < 2:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
     try:
         f = open("README.md", "r")
         exit(0)
     except IOError:
         print("Missing {}".format(argv[1]))
     exit(1)
-    
+
 if __name__ == "__main__":
     main(sys.argv)
