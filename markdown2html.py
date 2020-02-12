45fe974e818a71873058237d30988d4b8a83f381
scurry222 <scurry222@gmail.com>
2020-02-12 09:59:10 -0800

    pep8 compliant, comments

--- markdown2html.py
+++ markdown2html.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python
+""" Markdown is awesome! All your README.md are made in Markdown,
+    but do you know how Github are rendering them?
+
+    It’s time to code a Markdown to HTML!
+"""
 import sys
 import os
 
+
 def main(argv):
+    """ main - script that ttakes an argument 2 strings:
+                first argument is the name of the markdown file.
+                second argument is the output file name.
+    """
     if len(argv) < 2:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         exit(1)
